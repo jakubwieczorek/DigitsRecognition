9a7fd3349b27b7aa5283d9c57e635fbd1ea81378
jakubwieczorek <jakub.wieczorek0101@gmail.com>
2020-10-21 21:41:04 +0200

    C implementation, some bug

---
+++ neural_network/1_hidden/784_128_10/C_implementation/main.c
@@ -0,0 +1,77 @@
+#include"w1.h"
+#include"w2.h"
+#include"b1.h"
+#include"b2.h"
+#include"sample.h"
+#include<math.h>
+#include<stdio.h>
+
+void test2(double inputs[], double weights_matrix[], int row, int col, double biases[], double output[])
+{
+	int i, j;
+	for(i=0; i<row; i++){
+        	for(j=0; j<col; j++){
+            		printf("%f ", *(weights_matrix + i*col + j)); // or better: printf("%d ", *matrix++);
+        	}
+       		printf("\n");
+	}
+}
+
+// weights_matrix[], not [][], because second dimension has to be specified
+void feedforward(double x[], double z[], double weights_matrix[], int ROW, int COL, double biases[], double y[], double (*activationFunction)(double))
+{
+	int o, i;
+	double cur_w;
+
+	for(o = 0; o < COL; o++)
+	{
+		for(i = 0; i < ROW`; i++)
+		{	
+			cur_w = *(weights_matrix + i*COL + o);
+			y[o] += cur_w * x[i];
+		}
+	
+		z[o] = y[o] + biases[o];
+	}
+
+	for(o = 0; o < COL; o++)
+	{
+		y[o] = (*activationFunction)(z[o]);
+	}
+}
+
+double ReLU(double x)
+{
+	if(x < 0) {
+		return 0.0;
+	} else {
+		return x;
+	}
+}
+
+double sigmoid(double x)
+{
+	return 1.0 / ((double) + exp(-x));
+}
+
+void think(double x[], double y[], double z[])
+{
+	double l1_output[128], z1[128];
+	feedforward(x        , z1, w1[0], 784, 128, b1, l1_output, &ReLU);
+	feedforward(l1_output, z,  w2[0], 128, 10,  b2, y, &sigmoid);
+}
+
+int main(void)
+{
+	double y[10];
+	double z[10];
+
+	think(seven, y, z);
+
+	for(int i = 0; i < 10; i++)
+	{
+		printf("%f\n", y[i]);
+	}
+
+	return 0;
+}
---
+++ neural_network/1_hidden/784_128_10/keras_sample_save.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import math
+from random import random
+import numpy as np
+import csv
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    
+    (train_data, train_labels), (test_data, test_labels) = keras.datasets.mnist.load_data()
+
+    np.savetxt('./sample.txt', test_data[0].flatten() / 255.0, fmt='%f')
+
+    plt.grid(False) 
+    plt.imshow(test_data[0], cmap=plt.cm.binary)
+    plt.show()
